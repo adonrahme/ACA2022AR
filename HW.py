a05ac6ce79d9bc0b1d6eeef21837bbebaf766a5f
Adon Rahme <adonrahme@Adons-MacBook-Pro.local>
2022-07-31 02:09:55 -0700

    File polished

--- HW.py
+++ HW.py
@@ -1,5 +1,6 @@
 # Adon Rahme
 # Adobe Career Academy 2022
 
+# This file has no utility. It has been created, for educational testing only.git update-index --no-assume-unchanged path/to/file
 if __name__ == "__main__":
     print('Hello World')
